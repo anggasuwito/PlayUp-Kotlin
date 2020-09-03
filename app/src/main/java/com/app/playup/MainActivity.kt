2c2eefa9e83ec61f4de71cdf461e830e4946c574
unknown <angga.q.suwito@gmail.com>
2020-09-04 01:07:39 +0800

    [UPDATE]add permission

--- app/src/main/java/com/app/playup/MainActivity.kt
+++ app/src/main/java/com/app/playup/MainActivity.kt
@@ -1,24 +1,50 @@
 package com.app.playup
 
+import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     val REQUEST_READ_STORAGE_PERMISSION = 201
+    val REQUEST_OPEN_CAMERA_PERMISSION = 202
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        checkStoragePermission()
+        firstStoragePermission()
     }
 
-    fun checkStoragePermission() {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_READ_STORAGE_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            secondCameraPermission()
+        }
+    }
+
+    fun firstStoragePermission() {
         if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(
                 arrayOf(
                     android.Manifest.permission.READ_EXTERNAL_STORAGE,
                     android.Manifest.permission.WRITE_EXTERNAL_STORAGE
                 ), REQUEST_READ_STORAGE_PERMISSION
+            )
+        }
+    }
+
+    fun secondCameraPermission() {
+        if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(
+                arrayOf(
+                    android.Manifest.permission.CAMERA
+                ), REQUEST_OPEN_CAMERA_PERMISSION
             )
         }
     }
